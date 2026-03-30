b76450dee2fffea195b900dba2d0a1f25735daf5
chinni-yugandhar <chinniyugandhar996@gmail.com>
2026-03-30 17:58:02 +0530

    Add files via upload

---
+++ modules/logic.py
@@ -0,0 +1,15 @@
+likes = 0
+
+comments = []
+
+
+def addlike():
+    global likes
+    likes+=1
+    return likes
+def addcomments(com=None):
+    comments.append(com)
+    return comments
+
+
+    
---
+++ modules/main.py
@@ -0,0 +1,34 @@
+#lg function name again and again
+'''import logic as lg
+print(lg.likes)
+print(lg.comments)
+
+print(lg.addlike())
+print(lg.addlike())
+print(lg.addlike())
+
+print(lg.addcomments("good"))
+'''
+#another method
+'''from logic import likes,comments,addlike,addcomments
+print(likes)
+print(comments)
+
+print(addlike())
+print(addlike())
+print(addlike())
+
+print(addcomments("good"))
+'''
+#another method
+from logic import *
+
+print(likes)
+print(comments)
+
+print(addlike())
+print(addlike())
+print(addlike())
+
+print(addcomments("good"))
+
